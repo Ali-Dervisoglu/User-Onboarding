c01a9f75d43a6df9c0fef40619a228fe441bde7a
Ali Dervisoglu <derya.dervisoglu@gmail.com>
2019-09-12 18:08:06 -0700

    Step 2 done

--- src/components/HumanForm.js
+++ src/components/HumanForm.js
@@ -3,16 +3,20 @@ import { withFormik, Form, Field } from 'formik';
 import * as yup from 'yup';
 import axios from 'axios';
 
-const HumanForm = () => {
+const HumanForm = ({ errors, touched, status }) => {
 
 return (
     <Form>
+        {touched.name && errors.name && <p className="error">{errors.name}</p>}
         <Field type="text" name="name" placeholder="Name" />
+        {touched.email && errors.email && <p className="error">{errors.email}</p>}
         <Field type="text" name="email" placeholder="E-mail" />
+        {touched.password && errors.password && <p className="error">{errors.password}</p>}
         <Field type="text" name="password" placeholder="Password" />
+        {touched.termsOfService && errors.termsOfService && <p className="error">{errors.termsOfService}</p>}
         <label>
         <span>Terms of Service</span>
-        <Field type="checkbox" name="terms-of-service" />
+        <Field type="checkbox" name="termsOfService" />
         <button type="submit">Submit</button>
       </label>
     </Form>
@@ -24,8 +28,16 @@ export default withFormik ({
     mapPropsToValues: (values) => {
         return {
             name: values.name || '',
-            email: values.email || ''
+            email: values.email || '',
+            password: values.password || '',
+            termsOfService: values.termsOfService || false
         }
-    }
+    },
+    validationSchema: yup.object().shape({
+        name: yup.string('Name must be a string!').required('Name is required!'),
+        email: yup.string('Email must be a string!').required('Email is required!'),
+        password: yup.string('Password must be a string!').required('Password is required!'),
+        termsOfService: yup.boolean().oneOf([true], 'Terms of Service must be accepted!')
+      })
     
 })(HumanForm);
